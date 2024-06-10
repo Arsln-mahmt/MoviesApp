4b0ba7230908b3ef09a7b86419915d51dbecdd90
Arsln-mahmt <arslnmahmt198@gmail.com>
2024-06-10 09:26:00 +0300

    Initial Commit

---
+++ FilmlerApp/data/entity/FilmlerCevap.swift
@@ -0,0 +1,14 @@
+//
+//  FilmlerCevap.swift
+//  FilmlerApp
+//
+//  Created by Mahmut Arslan on 24.05.2024.
+//
+
+import Foundation
+
+class FilmlerCevap : Codable {
+    var filmler : [Filmler]?
+    var success : Int?
+    
+}
